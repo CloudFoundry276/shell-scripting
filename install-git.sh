d465d82ce2aa5c7c7d260fa0990d2f581d7f0141
CloudFoundry276 <cloudfoundry276@gmail.com>
2023-12-09 16:57:28 +0530

    installation of git

---
+++ install-git.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#############################################################
+# Date: 12/09/2023
+# Author: Sata Pitalekar
+# Title: Git Installation
+# Description: In this shell script, we are detecting the OS 
+#              installed in ec2 & accordingly install git as 
+#              per respective OS.
+#############################################################
+
+echo "Git Installation:"
+if [ "$(uname)" == "Linux" ];
+then
+    echo "OS: Linux"
+    sudo yum install git -y
+elif [ "$(uname)" == "Darwin" ];
+then
+    echo "OS: MacOS"
+    brew install git
+else
+    echo "OS not found"
+fi
