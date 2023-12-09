a4ee16cc36ab45af44c3e16f3c98e0da02a65f80
CloudFoundry276 <cloudfoundry276@gmail.com>
2023-12-09 16:27:41 +0530

    shell script to display date & uptime of system

---
+++ date-uptime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#############################################################
+# Date: 12/09/2023
+# Author: Sata Pitalekar
+# Title: Display Date & Uptime
+# Description: In this shell script, we are display the date 
+#              & uptime of system i.e. current ec2 instance.
+#############################################################
+
+echo "Date & Uptime of system is as below:"
+date
+uptime
