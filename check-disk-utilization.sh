f816c92938b8e279631e031573a49fe8e0afb87c
CloudFoundry276 <cloudfoundry276@gmail.com>
2023-12-09 18:31:47 +0530

    shell script to check disk utilization

---
+++ check-disk-utilization.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#############################################################
+# Date: 12/09/2023
+# Author: Sata Pitalekar
+# Title: Check Disk Utilization
+# Description: In this shell script, we are checking the 
+#              utilization of disk of system.
+#############################################################
+
+echo "Check Disk Utilization in Linux System"
+disk_size=$(df -h | grep "/dev/xvda128" | awk '{print $5}' | cut -d '%' -f 1)
+echo '$disk_size% of disk is filled'
+if [ $disk_size -gt 80 ];
+then
+    echo 'disk is utilized more than 80%, expand or clean up disk'
+else
+    echo 'enough disk is available'
+fi
